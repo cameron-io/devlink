2d973f3c1ec32281f3f7e0279a20f2e18ca64947
Cameron Gallichan <camerongallichan@gmail.com>
2024-02-07 21:57:43 +0000

    update mongo document removal function

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -156,9 +156,9 @@ router.delete('/', auth, async (req, res) => {
     // Remove user posts
     await Post.deleteMany({ user: req.user.id });
     // Remove profile
-    await Profile.findOneAndRemove({ user: req.user.id });
+    await Profile.findOneAndDelete({ user: req.user.id });
     // Remove user
-    await User.findOneAndRemove({ _id: req.user.id });
+    await User.findOneAndDelete({ _id: req.user.id });
 
     res.json({ msg: 'User deleted' });
   } catch (err) {
