2357a9657d911e1823373184dba221e96785daa5
Cameron Gallichan <camerongallichan@gmail.com>
2024-02-19 23:14:32 +0000

    improved retrieval of access_token from cookies

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,12 +3,14 @@ const jwt = require('jsonwebtoken');
 module.exports = function (req, res, next) {
   // Get token from header
   const cookies = req.headers.cookie;
-  const token = cookies.split(/; /).filter((e) => e.match('access_token=') != null)[0].split('access_token=')[1];
+  const token_cookie = cookies.split('; ').filter((e) => e.match('access_token=') != null);
 
   // Check if no token
-  if (!token) {
+  if (token_cookie.length == 0) {
     return res.status(401).json({ msg: 'No token, authorization denied.' });
   }
+
+  const token = token_cookie[0].split('access_token=')[1];
 
   // Verify token
   try {
