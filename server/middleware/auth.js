df72b6bf4e4b5d64e07e94cab54092be8d085f1e
Cameron Gallichan <camerongallichan@gmail.com>
2024-02-19 23:49:43 +0000

    handle undefined cookie during auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken');
 module.exports = function (req, res, next) {
   // Get token from header
   const cookies = req.headers.cookie;
+  if (!cookies) return res.status(401).json({ msg: 'No cookies provided.' });
+
   const token_cookie = cookies.split('; ').filter((e) => e.match('access_token=') != null);
 
   // Check if no token
