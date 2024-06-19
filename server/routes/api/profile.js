9afd41496d9f9127a4614b509131f8cda8f1f1af
Cameron Gallichan <camerongallichan@gmail.com>
2024-06-19 21:26:16 +0100

    convert skills formdata to list on client

--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -73,9 +73,7 @@ router.post(
     if (bio) profileFields.bio = bio;
     if (status) profileFields.status = status;
     if (githubusername) profileFields.githubusername = githubusername;
-    if (skills) {
-      profileFields.skills = skills.split(',').map((skill) => skill.trim());
-    }
+    if (skills) profileFields.skills = skills;
 
     // Initialize social object
     profileFields.social = {};
