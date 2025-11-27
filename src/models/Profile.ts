6205733f6d7554699c9e2e161477dafadd39ace9
Cameron Gallichan <camerongallichan@gmail.com>
2025-11-27 11:09:32 +0000

    update github-username property

--- src/models/Profile.ts
+++ src/models/Profile.ts
@@ -25,7 +25,7 @@ const ProfileSchema = new mongoose.Schema({
     bio: {
         type: String,
     },
-    githubusername: {
+    gitHubUsername: {
         type: String,
     },
     experience: [
